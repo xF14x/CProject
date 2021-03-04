c7cea70d09d824ca20961c45421ded8dba827ff3
xF14x <>
2021-03-04 18:16:32 +0300

    import the liberes and programm the locaton code

---
+++ main.py
@@ -0,0 +1,11 @@
+# programming by suliman almohawis \ twitter : F14Commander Inatagram : f14.commander
+
+# =-=-=-=-=-=-=-=-= import the liberes
+
+from os import system
+from sys import argv
+
+# =-=-=-=-=-=-=-=-=
+
+locaton_txt = open('locaton.txt')
+FileLocaton = locaton_txt.read()
